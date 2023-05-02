cad471b6f57eb27d8106b69b29d0801a3a89fb6b
fabbitox <hyunjin_11@naver.com>
2023-05-02 14:40:38 +0900

    update timetable

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 const timetable = [
-    [{ "date": "4월 24일", "contents": ["홍봉희", "자료구조", "이상현", "DB SQL"] },
-    { "date": "4월 25일", "contents": ["김경민", "React", "이상현", "DB SQL"] },
-    { "date": "4월 26일", "contents": ["홍봉희", "자료구조", "이상현", "DB SQL"] },
-    { "date": "4월 27일", "contents": ["김경민", "React", "이상현", "DB SQL"] },
-    { "date": "4월 28일", "contents": ["이상현", "DB SQL", "김경민", "React"] }],
     [{ "date": "5월 1일", "contents": ["근로자의 날"] },
     { "date": "5월 2일", "contents": ["김경민", "React", "이상현", "DB SQL"] },
     { "date": "5월 3일", "contents": ["홍봉희", "자료구조", "이상현", "DB SQL"] },
@@ -19,6 +14,21 @@ const timetable = [
     { "date": "5월 17일", "contents": ["이상현", "Servlet JSP", "홍봉희", "자료구조"] },
     { "date": "5월 18일", "contents": ["김경민", "React", "이상현", "Servlet JSP"] },
     { "date": "5월 19일", "contents": ["이상현", "Servlet JSP", "홍봉희", "자료구조"] }],
+    [{ "date": "5월 22일", "contents": ["홍봉희", "자료구조", "이상현", "Servlet JSP"] },
+    { "date": "5월 23일", "contents": ["김경민", "React", "이상현", "Servlet JSP"] },
+    { "date": "5월 24일", "contents": ["홍봉희", "자료구조", "이상현", "Servlet JSP"] },
+    { "date": "5월 25일", "contents": ["김경민", "React", "이상현", "Servlet JSP"] },
+    { "date": "5월 26일", "contents": ["김경민", "React", "홍봉희", "자료구조"] }],
+    [{ "date": "5월 29일", "contents": ["석가탄신일<br>대체공휴일"] },
+    { "date": "5월 30일", "contents": ["김경민", "React", "이상현", "Servlet JSP"] },
+    { "date": "5월 31일", "contents": ["홍봉희", "자료구조", "이상현", "Servlet JSP"] },
+    { "date": "6월 1일", "contents": ["이상현", "Servlet JSP", "이태훈", "Python"] },
+    { "date": "6월 2일", "contents": ["이상현", "Servlet JSP", "이태훈", "Python"] }],
+    [{ "date": "6월 5일", "contents": ["홍봉희", "자료구조", "이태훈", "Python"] },
+    { "date": "6월 6일", "contents": ["현충일"] },
+    { "date": "6월 7일", "contents": ["홍봉희", "자료구조", "이상현", "Spring Boot"] },
+    { "date": "6월 8일", "contents": ["이상현 ~2", "Spring Boot", "3~ 이태훈", "Python"] },
+    { "date": "6월 9일", "contents": ["이상현", "Spring Boot", "이태훈", "Python"] }],
 ];
 
 document.addEventListener('DOMContentLoaded', () => {
